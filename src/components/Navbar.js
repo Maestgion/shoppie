d9bdf3e6e038b62f38b58de7a7bd27b5acfbb27d
Abhishek Dubey <7abhishek1410@gmail.com>
2023-01-04 01:38:01 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+height: 60px;
+
+
+`
 
 const Navbar = () => {
   return (
     <>
-    
+        <Container>
+            navbar
+        </Container>
     </>
   )
 }
