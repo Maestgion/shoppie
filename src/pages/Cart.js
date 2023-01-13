d3f8f990e8e1dfb7c0bde25b9126c215cb0fa87e
Abhishek Dubey <7abhishek1410@gmail.com>
2023-01-13 15:19:21 +0530

    updates

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -50,6 +50,72 @@ justify-content: space-between;
 const Info = styled.div`
 flex: 3;
 `
+
+const Product = styled.div`
+display: flex;
+justify-content: space-between;
+`
+
+const ProductDetail = styled.div`
+flex: 2;
+display: flex;
+`
+
+const Image = styled.img`
+width: 250px;
+`
+
+const Details = styled.div`
+padding: 20px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+
+`
+const ProductName = styled.span``
+
+const ProductId = styled.span`
+`
+const ProductColour = styled.div`
+width: 30px;
+height: 30px;
+border-radius: 50%;
+background-color: ${(props)=>props.bg};
+` 
+
+const ProductSize = styled.span``
+
+const PriceDetail = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items:center;
+gap: 20px;
+`
+
+const AmountContainer = styled.div`
+display: flex;
+align-items: center;
+
+`
+
+const Amount = styled.div`
+margin: 5px;
+font-size: 25px;
+`
+
+const Price = styled.div`
+font-size: 30px;
+font-weight: 300;
+`
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 2px;
+`;
+
 const Summary = styled.div`
 flex: 1;
 `
@@ -73,10 +139,29 @@ const Cart = () => {
             </Top>
             <Bottom>
                 <Info>
-                    
+                    <Product>
+                        <ProductDetail>
+                            <Image src="https://www.freepnglogos.com/uploads/shoes-png/shoes-wasatch-running-3.png" />
+                            <Details>
+                                <ProductName><b>Product:</b> ROCKY SHOES</ProductName>
+                                <ProductId><b>ID:</b> 987658765</ProductId>
+                                <ProductColour bg={"black"} />
+                                <ProductSize><b>Size:</b> 37.5</ProductSize>
+                            </Details>
+                        </ProductDetail>
 
+                        <PriceDetail>
+                        <AmountContainer>
+                        <Add/>
+                            <Amount><b>3</b></Amount>
+                            <Remove/>
+                        </AmountContainer>
 
+                        <Price>RS. 2000</Price>
+                        </PriceDetail>
+                    </Product>
                     
+                    <Hr />
 
                 </Info>
                 <Summary>summary</Summary>
