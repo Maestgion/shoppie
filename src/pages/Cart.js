68070efa47bbf02f79a8b0974f8975d9c36af83d
Abhishek Dubey <7abhishek1410@gmail.com>
2023-01-13 15:38:00 +0530

    cart-ui-completed

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -49,11 +49,13 @@ justify-content: space-between;
 
 const Info = styled.div`
 flex: 3;
+
 `
 
 const Product = styled.div`
 display: flex;
 justify-content: space-between;
+margin: 10px 0px;
 `
 
 const ProductDetail = styled.div`
@@ -113,13 +115,40 @@ font-weight: 300;
 const Hr = styled.hr`
   background-color: #eee;
   border: none;
-  height: 2px;
+  height: 1px;
 `;
 
 const Summary = styled.div`
-flex: 1;
-`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 5px;
+  padding: 20px;
+  height: 50vh;
+`;
 
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
 
 const Cart = () => {
   return (
@@ -162,9 +191,52 @@ const Cart = () => {
                     </Product>
                     
                     <Hr />
+                    <Product>
+                        <ProductDetail>
+                            <Image src="https://www.freepnglogos.com/uploads/shoes-png/shoes-wasatch-running-3.png" />
+                            <Details>
+                                <ProductName><b>Product:</b> ROCKY SHOES</ProductName>
+                                <ProductId><b>ID:</b> 987658765</ProductId>
+                                <ProductColour bg={"black"} />
+                                <ProductSize><b>Size:</b> 37.5</ProductSize>
+                            </Details>
+                        </ProductDetail>
 
+                        <PriceDetail>
+                        <AmountContainer>
+                        <Add/>
+                            <Amount><b>3</b></Amount>
+                            <Remove/>
+                        </AmountContainer>
+
+                        <Price>RS. 2000</Price>
+                        </PriceDetail>
+                    </Product>
+                    
+                    <Hr />
                 </Info>
-                <Summary>summary</Summary>
+           
+
+                <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>Rs. 76</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>Rs. 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>Rs. -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>Rs. 87</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+                </Summary>
             </Bottom>
             </Wrapper>
         </Container>
