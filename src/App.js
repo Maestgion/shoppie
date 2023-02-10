3597372fc22b0dccd08d61d58a63bda30185236b
Abhishek Dubey <7abhishek1410@gmail.com>
2023-02-10 23:24:48 +0530

    initial-routing-done

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import React from 'react'
 import Cart from './pages/Cart'
 import Login from './pages/Login'
 import Register from './pages/Register'
-// import AllProducts from './pages/AllProducts'
+import AllProducts from './pages/AllProducts'
 import SingleProduct from './pages/SingleProduct'
-// import Home from "./pages/Home"
+import Home from "./pages/Home"
+import {BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
     <>
     
-    {/* <Home/> */}
-    {/* <AllProducts/> */}
-    {/* <SingleProduct/> */}
-    {/* <Register/> */}
-    {/* <Login/> */}
-    <Cart/>
+    <BrowserRouter>
+
+      <Routes>
+        <Route
+        path='/'
+        element={<Home/>}
+        />
+      </Routes>
+
+      <Routes>
+        <Route
+        path='/products/:category'
+        element={<AllProducts/>}
+        />
+      </Routes>
+
+      <Routes>
+        <Route
+        path='/product/:id'
+        element={<SingleProduct/>}
+        />
+      </Routes>
+
+      <Routes>
+        <Route
+        path='/cart'
+        element={<Cart/>}
+        />
+      </Routes>
+    
+  
+
+    </BrowserRouter>
     </>
   )
 }
