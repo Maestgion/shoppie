afe182dc5dafe37ebd952a6bc9f80d8e8280c205
Abhishek Dubey <7abhishek1410@gmail.com>
2023-03-19 13:33:57 +0530

    fixes

--- src/assets/axios/reqMethod.js
+++ src/assets/axios/reqMethod.js
@@ -5,7 +5,13 @@ import axios from "axios"
 const BASE_URL = "http://localhost:8000/api"
 
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.others.tokens.slice(-1)[0].token
-const TOKEN = ""
+const root = JSON.parse(localStorage.getItem("persist:root"))
+const user = root ? JSON.parse(root.user) : null 
+const currentUser = user ? user.currentUser : null
+const others = currentUser ? currentUser.others : null
+const tokens = others ? others.tokens:[];
+const lastToken = tokens.length>0?tokens.slice(-1)[0].token : ""
+const TOKEN = lastToken || ""
 
 export const publicReq = axios.create(
     {
